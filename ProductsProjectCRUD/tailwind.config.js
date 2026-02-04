f78da682f4035e7bcd19c8ce3af34e35b5bcc530
junaid sabir <junaidsabir2848@gmail.com>
2026-02-04 10:47:29 +0500

    installed daisyUI and flowbite

---
+++ ProductsProjectCRUD/tailwind.config.js
@@ -0,0 +1,19 @@
+﻿module.exports = {
+    content: [
+        "./Pages/**/*.cshtml",
+        "./Views/**/*.cshtml",
+        "./Shared/**/*.cshtml",
+        "./wwwroot/**/*.js",
+        "./node_modules/flowbite/**/*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require("daisyui"),
+        require("flowbite/plugin")
+    ],
+    daisyui: {
+        themes: ["light", "dark", "corporate", "emerald"],
+    }
+}
